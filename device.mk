cc2b8ef74a289f95064947a9f1af02a0391e5bd0
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-03-13 22:44:12 +0100

    Add device specific audio_tuning_mixer.txt
    Change-Id: I16356e5b7be71e6b875e774dbc68b79f11c5cd07

--- device.mk
+++ device.mk
@@ -57,6 +57,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml
 
 # Fingerprint sensor
