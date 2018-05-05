eb4b3820ccacf890080d5166173550c02204d8f8
Alexander Martinz <alex@amartinz.at>
2018-05-05 18:34:45 +0200

    Commonize config.fs
    Change-Id: I1df48399211703e344f3e2e1084f800fbb68f265

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,8 +33,5 @@ TARGET_OTA_ASSERT_DEVICE := dumpling
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
-# Filesystem
-TARGET_FS_CONFIG_GEN += $(DEVICE_PATH)/config.fs
-
 # inherit from the proprietary version
 -include vendor/oneplus/dumpling/BoardConfigVendor.mk
